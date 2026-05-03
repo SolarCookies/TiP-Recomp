9908ea263b5da574c1ae71688a1a43b0efd67dc9
SolarCookies <mag1182@rocketmail.com>
2026-05-03 05:02:53 -0500

    More build stuff

--- generated/retip_init.cpp
+++
@@ -1,8 +0,0 @@
-
-//=============================================================================
-// ReXGlue Generated - retip Function Mapping Table
-//=============================================================================
-
-#include "retip_init.h"
-
-PPCFuncMapping PPCFuncMappings[] = {};
--- generated/retip_init.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include "retip_config.h"
-#include <rex/ppc.h>
-#include <rex/logging.h>  // For REX_FATAL on unresolved calls
-
-
-// Function mapping table - iterate to register functions with FunctionDispatcher
